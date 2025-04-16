734af485c894487fde2aaf7926a7e2ec6aabd744
Mohamad Elhadidy <elhadidy384@gmail.com>
2025-04-16 15:59:28 +0200

    fix font error subset missing

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,8 +3,10 @@ import { Montserrat } from "next/font/google";
 
 export const raleway = Raleway({
     weight: ['600'],
+    subsets: ['latin'],
   });   
   
 export const montserrat = Montserrat({
     weight: ['700'],
+    subsets: ['latin'],
   });
