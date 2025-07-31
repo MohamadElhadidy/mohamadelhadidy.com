9e4711c3488a0d32868937a95054cca4b5ed4e29
Mohamad Elhadidy <elhadidy384@gmail.com>
2025-07-31 14:26:49 +0300

    Update data.ts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -10,15 +10,15 @@ interface Project {
   }
 
 export const projectsData : Project[] = [
-    // {
-    //     id: 1,
-    //     image: '/project.png',
-    //     title: 'FoodSure Website UI',
-    //     description: 'A clean and intuitive UI design for a fast food delivery platform. Focused on enhancing user experience through simple navigation, quick checkout, and mobile responsiveness. Designed to make ordering food fast, fun, and frustration-free.',
-    //     URL: 'https://mohamadelhadidy.com/',
-    //     GitHub: 'https://github.com/MohamadElhadidy',
-    //     Behance: 'https://www.behance.net/mohamadelhadidy98',
-    //     Dribble: 'https://dribbble.com/MohamadElhadidy',
-    // }
+    {
+        id: 1,
+        image: '/project.png',
+        title: 'FoodSure Website UI',
+        description: 'A clean and intuitive UI design for a fast food delivery platform. Focused on enhancing user experience through simple navigation, quick checkout, and mobile responsiveness. Designed to make ordering food fast, fun, and frustration-free.',
+        URL: 'https://mohamadelhadidy.com/',
+        GitHub: 'https://github.com/MohamadElhadidy',
+        Behance: 'https://www.behance.net/mohamadelhadidy98',
+        Dribble: 'https://dribbble.com/MohamadElhadidy',
+    }
 
 ]
